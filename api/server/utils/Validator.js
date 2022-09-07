00abd32107c0b30ae9f29ca5f068f0adad351f3b
Dung Tran Viet <dungztranviet.frontend.dev@gmail.com>
2022-09-07 21:45:45 +0700

    refactor code

--- api/server/utils/Validator.js
+++ api/server/utils/Validator.js
@@ -1,21 +1,22 @@
 import ResponseUtils from "./ResponseUtils";
+import ObjectUtils from './ObjectUtils';
 
 const responseUtils = new ResponseUtils();
 export default class Validator {
     static isParameterString(req, res, next) {
         const { name } = req.params;
-        if (!isNaN(name)) responseUtils.sendResponseErrorWhenRequestInvalid(res, 400, 'Category name must be a string');
+        if (!isNaN(name)) return responseUtils.sendResponseErrorWhenRequestInvalid(res, 400, 'Category name must be a string');
         next();
     }
 
     static isParameterNumber(req, res, next) {
         const { id } = req.params;
-        if (!Number(id)) responseUtils.sendResponseErrorWhenRequestInvalid(res, 400, 'Please input a valid numeric value');
+        if (!Number(id)) return responseUtils.sendResponseErrorWhenRequestInvalid(res, 400, 'Please input a valid numeric value');
         next();
     }
 
     static isRequestBodyEmpty(req, res, next) {
-        if (!ObjectUtils.isObjectNotEmpty(req.body)) responseUtils.sendResponseErrorWhenRequestInvalid(res);
+        if (!ObjectUtils.isObjectNotEmpty(req.body)) return responseUtils.sendResponseErrorWhenRequestInvalid(res);
         next();
     }
 }
